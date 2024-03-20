f5cdbff13135f1e6d9dc78c887904ba87daeea0d
tomcr00ze <tomcroose101@gmail.com>
2024-03-20 20:44:17 +0530

    added main() and crawlPage() with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,24 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`);
+    try {
+        const resp = await fetch(currentURL);
+        if (resp.status > 399) {
+            console.error(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`);
+            return
+        }
+        const contentType = resp.headers.get('content-type');
+        if (!contentType.includes('text/html')) {
+            console.error(`Non HTML response, Content-Type: ${contentType} on page: ${currentURL}`);
+            return
+        }
+        console.log(await resp.text());
+    } catch(err) {
+        console.error(`Error in fetching: ${err.message}, on page: ${currentURL}`);
+    }
+}
+
 // This function takes two arguments - 
 //      (string) : htmlBody -> contains the html code where we will find the URLs.
 //      (string) : baseURL  -> contains the URL of the website that we will crawl.
@@ -43,5 +62,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 };
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// console.log("Hi tomcr00ze !!!")
+const {crawlPage} = require('./crawl.js');
+
+function main() {
+    // Here 3 is used because : 
+    //      - the 1st argument is the interpreter(or compiler in case of compiled language).
+    //      - the 2nd argument is the program file name(with location).
+    // The first two arguments are for the shell not for the script point of view.
+    // E.g. -> node main.js argv1 argv2 ...
+    if(process.argv.length < 3) {
+        console.error("No website provided");
+        process.exit(1);
+    }
+    if(process.argv.length > 3) {
+        console.error("Too many command line arguments");
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2];
+    console.log(`Starting Crawl of ${baseURL}`);
+    crawlPage(baseURL);
+}
+
+main()
