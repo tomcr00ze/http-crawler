0db0af74ff58351ba34a1fd89a9aa60aadf9a489
tomcr00ze <tomcroose101@gmail.com>
2024-03-20 14:54:23 +0530

    added getURLsFromHTML() and its tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const {JSDOM} = require('jsdom')
+
+// This function takes two arguments - 
+//      (string) : htmlBody -> contains the html code where we will find the URLs.
+//      (string) : baseURL  -> contains the URL of the website that we will crawl.
+// Gets all URLs and links embedded in the webpage (htmlBody) and returns an array of URLs.
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0,1) === '/') {
+            // relative url
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`);
+                urls.push(urlObj.href);
+            } catch(err) {
+                console.error(err.message);
+            }
+        } else {
+            // absolute url
+            try {
+                const urlObj = new URL(linkElement.href);
+                urls.push(urlObj.href);
+            } catch(err) {
+                console.error(err.message);
+            }
+        }
+    }
+    return urls;
+}
+
 // This file will contain code to normalize the url
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
@@ -9,4 +41,7 @@ function normalizeURL(urlString) {
     return hostPath;
 }
 
-module.exports = {normalizeURL};
+module.exports = {
+    normalizeURL,
+    getURLsFromHTML
+};
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js');
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js');
 const {test, expect} = require('@jest/globals');
 
 // testing url with https://
@@ -30,5 +30,76 @@ test('normalizeURL strip http', () => {
     const input = 'http://BLOG.tomcrooze.dev/path';
     const actual = normalizeURL(input);
     const expected = 'blog.tomcrooze.dev/path';
+    expect(actual).toEqual(expected);
+});
+
+// testing getURLsFromHTML absolute path
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.tomcrooze.dev/">
+                tomcr00ze.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.tomcrooze.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.tomcrooze.dev/"];
+    expect(actual).toEqual(expected);
+});
+
+// testing getURLsFromHTML relative path
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">
+                tomcr00ze.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.tomcrooze.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.tomcrooze.dev/path/"];
+    expect(actual).toEqual(expected);
+});
+
+// testing getURLsFromHTML both paths
+test('getURLsFromHTML both', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.tomcrooze.dev/path1/">
+                tomcr00ze.dev Blog 1
+            </a>
+            <a href="/path2/">
+                tomcr00ze.dev Blog 2
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.tomcrooze.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.tomcrooze.dev/path1/", "https://blog.tomcrooze.dev/path2/"];
+    expect(actual).toEqual(expected);
+});
+
+// testing getURLsFromHTML invalid URL
+test('getURLsFromHTML invalid url', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="http.invalid.url">
+                Invalid URL
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.tomcrooze.dev";
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
     expect(actual).toEqual(expected);
 });
